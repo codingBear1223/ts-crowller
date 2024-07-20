aa3992be60e19ca7b4bfd9aa7f6054804f55c770
codingBear1223 <18054669370@163.com>
2024-07-20 20:11:32 +0800

    组合设计模式优化

---
+++ src/contentAnalyzer.ts
@@ -0,0 +1,55 @@
+import SuperAgent from "superagent"
+import * as cheerio  from "cheerio"
+import fs from "fs"
+import path from "path"
+
+interface CourseInfo {
+    title: string
+    count: number
+  }
+  
+  interface CourseResult {
+    time: number
+    courseInfos: CourseInfo[]
+  }
+  
+  interface Content {
+    [time: string]: CourseInfo[]
+  }
+export default class ContentAnalyzer {
+  constructor() {
+    
+  }
+
+  private getPageInfo = (html: string) => {
+    const courseInfos: CourseInfo[] = []
+    const $ = cheerio.load(html)
+    const courseItems = $('.course-item')
+    courseItems.map((index,item) => {
+      const descs = $(item).find('.course-desc')
+      const title = descs.eq(0).text()
+      const count = parseInt(descs.eq(1).text().split('：')[1], 10)
+      console.log(title, count)
+      courseInfos.push({ title, count })
+    })
+    const result = {time: (new Date()).getTime(), courseInfos}
+    return result
+  }
+
+  parseJson = (pageInfo: CourseResult, filePath:string) => {
+    let fileContent: Content = {}
+    if(fs.existsSync(filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+    }
+    
+    fileContent[pageInfo.time] = pageInfo.courseInfos
+    console.log(fileContent)
+    return fileContent
+  }
+
+  public analyze(html: string, path:string) {
+    const pageInfo = this.getPageInfo(html)
+    const fileContent = this.parseJson(pageInfo, path)
+    return JSON.stringify(fileContent)
+  }
+}
--- src/crowller.ts
+++ src/crowller.ts
@@ -2,65 +2,33 @@ import SuperAgent from "superagent"
 import * as cheerio  from "cheerio"
 import fs from "fs"
 import path from "path"
+import ContentAnalyzer from "./contentAnalyzer"
 
-interface CourseInfo {
-  title: string
-  count: number
-}
 
-interface CourseResult {
-  time: number
-  courseInfos: CourseInfo[]
-}
-
-interface Content {
-  [time: string]: CourseInfo[]
-}
 class Crowller {
     private secret = "123456"
-    
+    private filePath = path.resolve(__dirname, './data/course.json')
     private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`
     private rawHtml = ''
 
+    initSpiderprocess = async () => {
+      await this.getRawHtml() 
+      this.writeFile()
+    }
     getRawHtml = async () => {
       const result = await SuperAgent.get(this.url)
       this.rawHtml = result.text
-      const pageInfo = this.getPageInfo()
-      this.parseJson(pageInfo)
     }
 
-    getPageInfo = () => {
-      const courseInfos: CourseInfo[] = []
-      const $ = cheerio.load(this.rawHtml)
-      const courseItems = $('.course-item')
-      courseItems.map((index,item) => {
-        const descs = $(item).find('.course-desc')
-        const title = descs.eq(0).text()
-        const count = parseInt(descs.eq(1).text().split('：')[1], 10)
-        console.log(title, count)
-        courseInfos.push({ title, count })
-      })
-      const result = {time: (new Date()).getTime(), courseInfos}
-      return result
+    writeFile = () => {
+      const Content = this.analyzer.analyze(this.rawHtml, this.filePath)
+      fs.writeFileSync(this.filePath, Content)
     }
 
-    parseJson = (pageInfo: CourseResult) => {
-      
-      const filePath = path.resolve(__dirname, './data/course.json')
-      let fileContent: Content = {}
-      if(fs.existsSync(filePath)) {
-        fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
-      }
-      
-      fileContent[pageInfo.time] = pageInfo.courseInfos
-      console.log(fileContent)
-      fs.writeFileSync(filePath, JSON.stringify(fileContent))
-    }
-
-
-    constructor() {
-      this.getRawHtml()    
+    constructor(private analyzer: ContentAnalyzer) {
+        this.initSpiderprocess() 
     }
 }
 
-const crowller = new Crowller()
+const analyzer = new ContentAnalyzer()
+const crowller = new Crowller(analyzer)
