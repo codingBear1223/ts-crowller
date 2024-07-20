ce1a4242773fd90b4627596d58142c22b32fa385
codingBear1223 <18054669370@163.com>
2024-07-20 17:30:48 +0800

    爬取内容存入文件

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,18 +1,32 @@
 import SuperAgent from "superagent"
 import * as cheerio  from "cheerio"
+import fs from "fs"
+import path from "path"
 
 interface CourseInfo {
   title: string
   count: number
+}
+
+interface CourseResult {
+  time: number
+  courseInfos: CourseInfo[]
+}
+
+interface Content {
+  [time: string]: CourseInfo[]
 }
 class Crowller {
     private secret = "123456"
     
     private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`
     private rawHtml = ''
+
     getRawHtml = async () => {
       const result = await SuperAgent.get(this.url)
       this.rawHtml = result.text
+      const pageInfo = this.getPageInfo()
+      this.parseJson(pageInfo)
     }
 
     getPageInfo = () => {
@@ -27,10 +41,25 @@ class Crowller {
         courseInfos.push({ title, count })
       })
       const result = {time: (new Date()).getTime(), courseInfos}
+      return result
     }
+
+    parseJson = (pageInfo: CourseResult) => {
+      
+      const filePath = path.resolve(__dirname, './data/course.json')
+      let fileContent: Content = {}
+      if(fs.existsSync(filePath)) {
+        fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+      }
+      
+      fileContent[pageInfo.time] = pageInfo.courseInfos
+      console.log(fileContent)
+      fs.writeFileSync(filePath, JSON.stringify(fileContent))
+    }
+
+
     constructor() {
-      this.getRawHtml()
-      this.getPageInfo()
+      this.getRawHtml()    
     }
 }
 
