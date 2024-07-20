02da14da96be187c7180ee8e6d3ec93599506410
codingBear1223 <18054669370@163.com>
2024-07-20 21:14:42 +0800

    分析器使用单例模式

--- src/contentAnalyzer.ts
+++ src/contentAnalyzer.ts
@@ -1,54 +1,60 @@
-import * as cheerio  from "cheerio"
-import fs from "fs"
-import {Analyzer} from "./crowller"
+import * as cheerio from "cheerio";
+import fs from "fs";
+import { Analyzer } from "./crowller";
 
 interface CourseInfo {
-    title: string
-    count: number
-  }
-  
-  interface CourseResult {
-    time: number
-    courseInfos: CourseInfo[]
-  }
-  
-  interface Content {
-    [time: string]: CourseInfo[]
-  }
+  title: string;
+  count: number;
+}
+
+interface CourseResult {
+  time: number;
+  courseInfos: CourseInfo[];
+}
+
+interface Content {
+  [time: string]: CourseInfo[];
+}
 export default class ContentAnalyzer implements Analyzer {
-  constructor() {
-    
-  }
+  private constructor() {}
+
+  private static instance: ContentAnalyzer;
+  public static getInstance = () => {
+    if (!ContentAnalyzer.instance) {
+      ContentAnalyzer.instance = new ContentAnalyzer();
+    }
+    return ContentAnalyzer.instance;
+  };
 
   private getPageInfo = (html: string) => {
-    const courseInfos: CourseInfo[] = []
-    const $ = cheerio.load(html)
-    const courseItems = $('.course-item')
-    courseItems.map((index,item) => {
-      const descs = $(item).find('.course-desc')
-      const title = descs.eq(0).text()
-      const count = parseInt(descs.eq(1).text().split('：')[1], 10)
-      console.log(title, count)
-      courseInfos.push({ title, count })
-    })
-    const result = {time: (new Date()).getTime(), courseInfos}
-    return result
-  }
+    const courseInfos: CourseInfo[] = [];
+    const $ = cheerio.load(html);
+    const courseItems = $(".course-item");
+    courseItems.map((index, item) => {
+      const descs = $(item).find(".course-desc");
+      const title = descs.eq(0).text();
+      const count = parseInt(descs.eq(1).text().split("：")[1], 10);
+      console.log(title, count);
+      courseInfos.push({ title, count });
+    });
+    const result = { time: new Date().getTime(), courseInfos };
+    return result;
+  };
 
-  parseJson = (pageInfo: CourseResult, filePath:string) => {
-    let fileContent: Content = {}
-    if(fs.existsSync(filePath)) {
-      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+  private parseJson = (pageInfo: CourseResult, filePath: string) => {
+    let fileContent: Content = {};
+    if (fs.existsSync(filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(filePath, "utf-8"));
     }
-    
-    fileContent[pageInfo.time] = pageInfo.courseInfos
-    console.log(fileContent)
-    return fileContent
-  }
 
-  public analyze(html: string, path:string) {
-    const pageInfo = this.getPageInfo(html)
-    const fileContent = this.parseJson(pageInfo, path)
-    return JSON.stringify(fileContent)
+    fileContent[pageInfo.time] = pageInfo.courseInfos;
+    console.log(fileContent);
+    return fileContent;
+  };
+
+  public analyze(html: string, path: string) {
+    const pageInfo = this.getPageInfo(html);
+    const fileContent = this.parseJson(pageInfo, path);
+    return JSON.stringify(fileContent);
   }
 }
--- src/crowller.ts
+++ src/crowller.ts
@@ -1,37 +1,37 @@
-import SuperAgent from "superagent"
-import fs from "fs"
-import path from "path"
-import ContentAnalyzer from "./contentAnalyzer"
+import SuperAgent from "superagent";
+import fs from "fs";
+import path from "path";
+import ContentAnalyzer from "./contentAnalyzer";
 
- export interface Analyzer {
-  analyze(html: string, filePath:string): string
+export interface Analyzer {
+  analyze(html: string, filePath: string): string;
 }
 
 class Crowller {
-    private filePath = path.resolve(__dirname, './data/course.json')
-    private rawHtml = ''
+  private filePath = path.resolve(__dirname, "./data/course.json");
+  private rawHtml = "";
 
-    initSpiderprocess = async () => {
-      await this.getRawHtml() 
-      this.writeFile()
-    }
-    getRawHtml = async () => {
-      const result = await SuperAgent.get(this.url)
-      this.rawHtml = result.text
-    }
+  initSpiderprocess = async () => {
+    await this.getRawHtml();
+    this.writeFile();
+  };
+  getRawHtml = async () => {
+    const result = await SuperAgent.get(this.url);
+    this.rawHtml = result.text;
+  };
 
-    writeFile = () => {
-      const Content = this.analyzer.analyze(this.rawHtml, this.filePath)
-      fs.writeFileSync(this.filePath, Content)
-    }
+  writeFile = () => {
+    const Content = this.analyzer.analyze(this.rawHtml, this.filePath);
+    fs.writeFileSync(this.filePath, Content);
+  };
 
-    constructor(private analyzer: Analyzer, private url: string) {
-        this.initSpiderprocess() 
-    }
+  constructor(private analyzer: Analyzer, private url: string) {
+    this.initSpiderprocess();
+  }
 }
 
-const secret = "123456"
-const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`
+const secret = "123456";
+const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
 
-const analyzer = new ContentAnalyzer()
-new Crowller(analyzer, url)
+const analyzer = ContentAnalyzer.getInstance();
+new Crowller(analyzer, url);
