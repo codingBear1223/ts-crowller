253742d733530285bd649aa6183deb11968ae815
codingBear1223 <18054669370@163.com>
2024-07-23 23:07:41 +0800

    利用工厂模式对构造函数扩展

--- src/decorator.ts
+++ src/decorator.ts
@@ -1,28 +1,25 @@
-function decorateFnc(constructor: any) {
-  //类装饰器接收的是构造函数
-  constructor.prototype.hello = function () {
-    console.log("ddddd");
+function decorateFnc() {
+  return function <T extends new (...args: any[]) => {}>(constructor: T) {
+    //构造函数可以有多个任意类型的参数 ...args
+    //泛型 T 继承了构造函数，具备了构造函数的能力
+    return class extends constructor {
+      age = 24;
+      name = "frfr";
+      getName() {
+        return this.name;
+      }
+    };
   };
 }
 
-function decorateCFnc(constructor: any) {
-  console.log("ccccc");
-}
-
-function decorateImdtFnc(flag: boolean) {
-  if (flag) {
-    return function (constructor: any) {
-      console.log("imdt tt");
-    };
-  } else {
-    return function (constructor: any) {
-      console.log("imdt ff");
-    };
+const Test = decorateFnc()(
+  class {
+    name: string;
+    constructor(name: string) {
+      this.name = name;
+    }
   }
-}
+);
 
-@decorateFnc
-@decorateCFnc
-@decorateImdtFnc(true)
-class Test {}
-(new Test() as any).hello();
+const ins = new Test("lili");
+console.log(ins.getName());
