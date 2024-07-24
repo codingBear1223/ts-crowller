3bcf68dab96d68c5e1016678ef71a82e206cad2c
codingBear1223 <18054669370@163.com>
2024-07-24 17:22:40 +0800

    访问器装饰器

--- src/decorator.ts
+++ src/decorator.ts
@@ -1,25 +1,25 @@
-// function decorateFnc() {
-//   return function <T extends new (...args: any[]) => {}>(constructor: T) {
-//     //构造函数可以有多个任意类型的参数 ...args
-//     //泛型 T 继承了构造函数，具备了构造函数的能力
-//     return class extends constructor {
-//       age = 24;
-//       name = "frfr";
-//       getName() {
-//         return this.name;
-//       }
-//     };
-//   };
-// }
+function decorateFnc() {
+  return function <T extends new (...args: any[]) => {}>(constructor: T) {
+    //构造函数可以有多个任意类型的参数 ...args
+    //泛型 T 继承了构造函数，具备了构造函数的能力
+    return class extends constructor {
+      age = 24;
+      name = "frfr";
+      getName() {
+        return this.name;
+      }
+    };
+  };
+}
 
-// const TestNew = decorateFnc()(
-//   class Test {
-//     name: string;
-//     constructor(name: string) {
-//       this.name = name;
-//     }
-//   }
-// );
+const TestNew = decorateFnc()(
+  class Test {
+    name: string;
+    constructor(name: string) {
+      this.name = name;
+    }
+  }
+);
 
 //函数装饰器
 //装饰普通方法时，target 是类的 prototype
@@ -56,7 +56,34 @@ class FncFactory {
 }
 
 //const ins = new TestNew("lili");
-const fnc = new FncFactory("jio jio");
+//const fnc = new FncFactory("jio jio");
 //fnc.getName = () => "frfr";    //descriptor修饰后不可以修改方法了
 //console.log(ins.getName());
-console.log(fnc.getName());
+//console.log(fnc.getName());
+
+function visitFactory(
+  target: any,
+  key: string,
+  descriptor: PropertyDescriptor
+) {
+  console.log("visitFactory=>", target, key);
+  //descriptor.writable = false;  writable是true的话，set name不能赋值
+}
+class privtFactory {
+  private _name: string;
+  constructor(name: string) {
+    this._name = name;
+  }
+  //get 和 set 不能同时用装饰器
+  get name() {
+    return this._name;
+  }
+
+  @visitFactory
+  set name(name: string) {
+    this._name = name;
+  }
+}
+
+const visit = new privtFactory("vvvv");
+visit.name = "change";
